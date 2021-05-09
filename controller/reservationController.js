dd4e5f39df0c554eab83dcb7aafb157a375fa9b8
ps3vdo <79896188191@ya.ru>
2021-05-09 08:36:32 +0300

    добавлено бронирование

--- controller/reservationController.js
+++ controller/reservationController.js
@@ -8,9 +8,9 @@ class ReservationController {
         try {
             const {place_id, date_start, date_end, id_owner = "", phone = ""} = req.body;
 
-            const {is_free} = (await db.query('SELECT * FROM place where id = $1', [place_id])).rows[0];
-            const isOwner = (await db.query('SELECT * FROM owner where id = $1', [id_owner])).rowCount;
-            const isReserved = (await db.query('SELECT * FROM reservation where place_id = $1', [place_id])).rowCount;
+            const {is_free} = (await db.query ('SELECT * FROM place where id = $1', [place_id])).rows[0];
+            const isOwner = (await db.query ('SELECT * FROM owner where id = $1', [id_owner])).rowCount;
+            const isReserved = (await db.query ('SELECT * FROM reservation where place_id = $1', [place_id])).rowCount;
             console.log(isReserved)
 
             if (!is_free) {
@@ -18,19 +18,19 @@ class ReservationController {
             }
 
             if (isReserved) {
-                return next(ApiError.badRequest("Place is reserved"));
+                return next(ApiError.badRequest(`Place ${place_id} already taken`));
             }
 
             if (!isOwner && phone === "") {
                 return next(ApiError.badRequest("Please enter phone or id owner"));
             }
-            else if (!isOwner && phone !== ""){
-                    const isPhone = phone.match(/^\d{10}$/g);
-                    if (!isPhone) return next(ApiError.badRequest('Invalid phone number'));
+            else if (!isOwner && phone !== "") {
+                const isPhone = phone.match(/^\d{10}$/g);
+                if (!isPhone) return next(ApiError.badRequest('Invalid phone number'));
             }
 
             if (!isOwner) {
-               const reservationAdd = await db.query(
+                const reservationAdd = await db.query(
                     'INSERT INTO reservation (place_id, date_start, date_end, phone) values ($1, $2, $3, $4) RETURNING * ',
                     [place_id, date_start, date_end, phone]);
                 res.json(reservationAdd.rows[0]);
@@ -41,12 +41,10 @@ class ReservationController {
                 res.json(reservationAdd.rows[0]);
             }
         } catch (e) {
-            console.log(e)
+            console.log(e);
             return next(ApiError.badRequest(e.message));
         }
-
     }
-
 }
 
 module.exports = new ReservationController()
